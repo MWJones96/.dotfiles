6c51ce517beccd0d567de5dfac59d1e8611539f8
Matthew Jones <mwjones0896@gmail.com>
2026-05-08 17:40:14 +0100

    Fixing stuff

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,19 +1,14 @@
 return {
   {
     "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
-
     opts = require "configs.conform",
   },
-
-  -- These are some examples, uncomment them if you want to see them work!
   {
     "neovim/nvim-lspconfig",
     config = function()
       require "configs.lspconfig"
     end,
   },
-
   {
     'mrcjkb/rustaceanvim',
     version = '^5',
@@ -22,19 +17,19 @@ return {
     config = function ()
       local mason_registry = require('mason-registry')
       local codelldb = mason_registry.get_package("codelldb")
-      local extension_path = codelldb:get_install_path() .. "/extension/"
+      local install_path = require('mason.settings').current.install_root_dir .. "/packages/codelldb"
+      local extension_path = install_path .. "/extension"
       local codelldb_path = extension_path .. "adapter/codelldb"
-      local liblldb_path = extension_path.. "lldb/lib/liblldb.dylib"
+      local liblldb_path = extension_path .. "lldb/lib/liblldb.dylib"
       local cfg = require('rustaceanvim.config')
 
       vim.g.rustaceanvim = {
         dap = {
-          adapter = cfg.get_codelldb_adapter(codelldb_path, liblldb_path),
+          adapter = cfg.get_codelldb_adapter(codelldb_path, liblldb_path)
         },
       }
     end
   },
-
   {
     'rust-lang/rust.vim',
     ft = "rust",
@@ -83,13 +78,4 @@ return {
       })
     end
   },
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
 }
