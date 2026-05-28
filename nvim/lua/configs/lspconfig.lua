880495d1948b367041d79b3317822144a447deb4
Matthew Jones <mwjones0896@gmail.com>
2026-05-28 13:43:10 +0100

    Fix LSP deprecation warnings

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -1,14 +1,12 @@
-require("nvchad.configs.lspconfig").defaults()
-
-local lspconfig = require "lspconfig"
 local nvim_lsp = require "nvchad.configs.lspconfig"
-
-local servers = { "html", "cssls", "rust_analyzer" }
+local servers = { "html", "cssls"}
 
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
+  vim.lsp.config(lsp, {
     on_attach = nvim_lsp.on_attach,
     on_init = nvim_lsp.on_init,
     capabilities = nvim_lsp.capabilities,
-  }
+  })
+
+  vim.lsp.enable(lsp)
 end
