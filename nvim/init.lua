32541235d5ec30e07737dcd875eea7dee0d6218c
Matthew Jones <mwjones0896@gmail.com>
2026-06-24 19:20:29 +0100

    Block arrow keys

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,19 @@
 vim.g.base46_cache = vim.fn.stdpath "data" .. "/base46/"
 vim.g.mapleader = " "
 
+vim.keymap.set({ "n", "i", "v" }, "<Up>", function()
+  vim.api.nvim_echo({ { "Use 'k' for up!", "WarningMsg" } }, false, {})
+end, { desc = "Nudge for k" })
+vim.keymap.set({ "n", "i", "v" }, "<Down>", function()
+  vim.api.nvim_echo({ { "Use 'j' for down!", "WarningMsg" } }, false, {})
+end, { desc = "Nudge for j" })
+vim.keymap.set({ "n", "i", "v" }, "<Left>", function()
+  vim.api.nvim_echo({ { "Use 'h' for left!", "WarningMsg" } }, false, {})
+end, { desc = "Nudge for h" })
+vim.keymap.set({ "n", "i", "v" }, "<Right>", function()
+  vim.api.nvim_echo({ { "Use 'l' for right!", "WarningMsg" } }, false, {})
+end, { desc = "Nudge for l" })
+
 -- bootstrap lazy and all plugins
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 
